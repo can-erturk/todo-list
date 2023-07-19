b6c57054e4b3314b20a02b52e7e6e468cb21e6d1
meecan <cannhann42@gmail.com>
2023-07-19 15:30:05 +0300

    Helper functions added.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,32 @@
-document.querySelectorAll('[data-link]').forEach(el => {
-    el.addEventListener('click', () => {
-        document.querySelector('#content .page.active').classList.remove('active')
+Object.prototype.$ = function(target, node = document) {
+    const query = node.querySelectorAll(target)
+    if (query.length !== 0) {
+        return query.length > 1 ? query : query[0]
+    }else {
+        return []
+    }
+}
+
+Object.prototype.on = function(e, handler) {
+    this.addEventListener(e, () => {
+        handler()
+    })
+}
+
+$('[data-link]').forEach(el => {
+    el.on('click', () => {
+        $('#content .page.active').classList.remove('active')
         
         const link = el.getAttribute('data-link')
-        document.querySelector(`.page[data-page=${link}]`).classList.add('active')
-
         const name = el.getAttribute('data-name')
-        document.querySelector('.page-header .title').innerText = name;
+        
+        $(`.page[data-page=${link}]`).classList.add('active')
+        $('.page-header .title').innerText = name;
     })
 })
 
-document.querySelectorAll('.sidebar-toggle').forEach(el => {
-    el.addEventListener('click', () => {
-        document.querySelector('#sidebar').classList.toggle('sidebar-open')
+$('.sidebar-toggle').forEach(el => {
+    el.on('click', () => {
+        $('#sidebar').classList.toggle('sidebar-open')
     })
 })
