66c1a935c1ffbffaf4b0c6e75f2743df29bc36b5
meecan <cannhann42@gmail.com>
2023-07-19 14:49:12 +0300

    Page layout changed.

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,5 +4,14 @@ document.querySelectorAll('[data-link]').forEach(el => {
         
         const link = el.getAttribute('data-link')
         document.querySelector(`.page[data-page=${link}]`).classList.add('active')
+
+        const name = el.getAttribute('data-name')
+        document.querySelector('.page-header .title').innerText = name;
+    })
+})
+
+document.querySelectorAll('.sidebar-toggle').forEach(el => {
+    el.addEventListener('click', () => {
+        document.querySelector('#sidebar').classList.toggle('sidebar-open')
     })
 })
