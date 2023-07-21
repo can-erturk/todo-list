51262782745a8119d597981acd739afb3b246f6e
meecan <cannhann42@gmail.com>
2023-07-21 13:09:55 +0300

    Helper functions removed.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,23 +1,7 @@
-// Element selector function
-Object.prototype.$ = function(target, node = document) {
-    const query = node.querySelectorAll(target)
-    if (query.length !== 0) {
-        return query.length > 1 ? query : query[0]
-    }else {
-        return []
-    }
-}
-
-// Event listener function
-Object.prototype.on = function(e, handler) {
-    this.addEventListener(e, () => {
-        handler()
-    })
-}
-
 // Sidebar open/close
-$('.sidebar-toggle').forEach(el => {
-    el.on('click', () => {
-        $('#sidebar').classList.toggle('sidebar-open')
+document.querySelectorAll('.sidebar-toggle').forEach(el => {
+    el.addEventListener('click', () => {
+        const sidebar = document.querySelector('#sidebar')
+        sidebar.classList.toggle('sidebar-open')
     })
 })
