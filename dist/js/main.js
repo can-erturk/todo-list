1f21a97b4fe3a5766380c39736dce169789dcc31
meecan <cannhann42@gmail.com>
2023-07-23 16:33:34 +0300

    List styles added.

--- dist/js/main.js
+++ dist/js/main.js
@@ -159,3 +159,9 @@ const sidebar = () => {
 window.addEventListener('DOMContentLoaded', () => {
     sidebar()
 })
+
+document.querySelectorAll('.list-content .list-item').forEach(el => {
+    el.addEventListener('click', function(params) {
+        el.classList.toggle('completed')
+    })
+});
